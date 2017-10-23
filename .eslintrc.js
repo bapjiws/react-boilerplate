6cb7f1f654580463a923877c753508de7453f3ec
Alex Boklin <alex.boklin@gmail.com>
2017-10-23 10:27:20 +0200

    Comment out unnecessary pieces of .eslintrc file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,41 @@
 module.exports = {
     "extends": [
         "airbnb",
-        "plugin:react/recommended",
+        // "eslint:recommended",
+        // "plugin:react/recommended",
         "prettier",
         "prettier/react"
     ],
 
     "plugins": [
         "react",
+        "jsx-a11y",
+        "import",
         "prettier"
     ],
 
     // "parser": "typescript-eslint-parser",
 
     "rules": {
-        "prettier/prettier":  ["error", {"singleQuote": true}],
-
+        // "prettier/prettier":  ["error", {"singleQuote": true}],
+        //
         "react/jsx-filename-extension": ["error", {"extensions": [".js", ".jsx"]}],
-
-        "import/extensions": ["error", "always", {
-            "js": "never",
-            "jsx": "never"
-        }],
+        //
+        // "import/extensions": ["error", "always", {
+        //     "js": "never",
+        //     "jsx": "never"
+        // }],
 
         "import/no-extraneous-dependencies": ["error", {"devDependencies": true}]
     },
-
-    "settings": {
-        "import/resolver": {
-            "node": {
-                "extensions": [".js", ".jsx"]
-            }
-        },
-
-        "import/core-modules": [ "autoprefixer" ]
-    }
+    //
+    // "settings": {
+    //     "import/resolver": {
+    //         "node": {
+    //             "extensions": [".js", ".jsx"]
+    //         }
+    //     },
+    //
+    //     "import/core-modules": [ "autoprefixer" ]
+    // }
 };
